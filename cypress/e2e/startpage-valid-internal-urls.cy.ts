d5e327684ba626eb4ceed79eb9680a8a84403969
Sven Marnitz <sven@testify.team>
2022-07-08 13:28:07 +0200

    iterate a tags once
    Co-authored-by: Lennard <lennard@nevercodealone.de>

--- cypress/e2e/startpage-valid-internal-urls.cy.ts
+++ cypress/e2e/startpage-valid-internal-urls.cy.ts
@@ -7,23 +7,30 @@ describe("Validate internal links", () => {
   it("Every page has status code 200 and no redirect", () => {
     const hostname = window.location.hostname.split(".").slice(-2).join(".");
 
+    let data: string[] = [];
+
     cy.get("a").each((item) => {
       const url = item.attr("href");
 
-      if (url.indexOf('mailto') == -1 && url && (url.startsWith("/") || url.includes(hostname))) {
-        cy.request({
-          url: url,
-          followRedirect: false,
-        }).then((resp) => {
-          expect(resp.status).to.eq(200);
-        });
-      } else {
-        cy.log("Skip item: " + url);
+
+      if (url !== undefined && Cypress._.indexOf(data, url) === -1) {
+        data.push(url);
+
+        if (url.indexOf('mailto') == -1 && url && (url.startsWith("/") || url.includes(hostname))) {
+          cy.request({
+            url: url,
+            followRedirect: false,
+          }).then((resp) => {
+            expect(resp.status).to.eq(200);
+          });
+        } else {
+          cy.log("Skip item: " + url);
+        }
       }
     });
   });
 
-  it('Validate that imprint is visable', () => {
+  it('Validate that imprint is visible', () => {
     cy.get('a').contains('Impressum').should('be.visible');
   })
 });
