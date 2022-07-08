feb771b75b57087fffbe301d8134d1a400d18e2f
Sven Marnitz <sven@testify.team>
2022-07-08 13:25:03 +0200

    add cypress exception handling
    Co-authored-by: Lennard <lennard@nevercodealone.de>

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,24 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, promise) => {
+  // we expect a 3rd party library error with message 'talk-chat'
+  // and don't want to fail the test so we return false
+  if (err.message.includes('o.removeEventListener')) {
+    return false;
+  }
+
+  // when the exception originated from an unhandled promise
+  // rejection, the promise is provided as a third argument
+  // you can turn off failing the test in this case
+  if (promise) {
+    return false;
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+});
